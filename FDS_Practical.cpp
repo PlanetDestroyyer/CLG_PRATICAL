56352359c954564fc17115e5026274b1cad2add5
Pranav <59405252+PlanetDestroyyer@users.noreply.github.com>
2023-12-14 20:45:26 +0530

    Update FDS_Practical.cpp

--- FDS_Practical.cpp
+++ FDS_Practical.cpp
@@ -3,62 +3,87 @@ using namespace std;
 
 #define MAX_JOBS 100
 
-class job_queue{
+class job_queue {
     int* arr;
     int front;
     int rare;
 
-    public:
-    job_queue(){
-        arr=new int[MAX_JOBS];
+public:
+    job_queue() {
+        arr = new int[MAX_JOBS];
         front = -1;
-        rare=-1;
+        rare = -1;
     }
 
-    void push(int x){
-        if(rare == MAX_JOBS-1){
-            cout<<"Queue is full"<<endl;
+    void push(int x) {
+        if (rare == MAX_JOBS - 1) {
+            cout << "Queue is full" << endl;
             return;
         }
 
         rare++;
-        arr[rare] = x; 
+        arr[rare] = x;
 
-        if(front == -1){
+        if (front == -1) {
             front++;
         }
     }
-    void pop(){
-        if (front==-1 || front>rare)
-        {
-            cout<<"No elements in queue"<<endl;
+
+    void pop() {
+        if (front == -1 || front > rare) {
+            cout << "No elements in queue" << endl;
             return;
         }
 
-        front++;        
+        front++;
     }
 
-    void display(){
-        if (front==-1 || front>rare)
-        {
-            cout<<"No elements in queue"<<endl;
+    void display() {
+        if (front == -1 || front > rare) {
+            cout << "No elements in queue" << endl;
             return;
         }
-        for(int i = front; i <= rare; i++){ 
-            cout<<arr[i]<<" ";
+        for (int i = front; i <= rare; i++) {
+            cout << arr[i] << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
 };
 
-
-int main(){
-    cout<<"Queue"<<endl;
+int main() {
+    cout << "Queue" << endl;
     job_queue job;
-    job.push(89);
-    job.push(98);
-    job.push(8);
-    job.pop();
-    job.display();
+
+    int ch, x;
+
+    do {
+        cout << "\n1.Insert Job\n2.Delete Job\n3.Display\n4.Exit\nEnter your choice: ";
+        cin >> ch;
+
+        switch (ch) {
+            case 1:
+                cout << "\nEnter data: ";
+                cin >> x;
+                job.push(x);
+                break;
+
+            case 2:
+                job.pop();
+                break;
+
+            case 3:
+                job.display();
+                break;
+
+            case 4:
+                cout << "Exiting Program.....";
+                break;
+
+            default:
+                cout << "Invalid choice. Please enter a valid option.";
+        }
+
+    } while (ch != 4);
+
     return 0;
 }
